9c961d3c9ff39b113d2595b6b8cc2798e9554708
Danail Donkov <danail.donkov5@gmail.com>
2025-10-07 00:05:03 +0300

    Create Find_The_SumDigits_in_Number.cpp

---
+++ Basics/Find_The_SumDigits_in_Number.cpp
@@ -0,0 +1,19 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    int sum = 0;
+    int n = abs(number);
+
+    while (n > 0) {
+        int digit = n % 10;
+        sum += digit;
+        n /= 10;
+    }
+    cout << "Sum of digits: " << sum << endl;
+    return 0;
+}
