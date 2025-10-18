a2dd5840724dafeea87a2be6755bf9b2f82dd2ca
Danail Donkov <danail.donkov5@gmail.com>
2025-10-18 21:49:42 +0300

    Update From_Degrees_to_Radians.cpp

--- Additional_Practice/Textbook_Tasks/From_Degrees_to_Radians.cpp
+++ Additional_Practice/Textbook_Tasks/From_Degrees_to_Radians.cpp
@@ -1,1 +1,21 @@
+# Да се напише програма за превръщане от градуси в радиани
 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+const double PI = 3.14;
+
+int main ()
+{
+    double alpha;
+    cout << "Enter the degrees: ";
+    cin >> alpha;
+    
+    double alpha_radians;
+    alpha_radians = PI / 180 * alpha;
+    
+    cout << fixed << setprecision(2);
+    cout << alpha << " Degrees equal to " << alpha_radians << " radians.";
+    return 0;
+}
