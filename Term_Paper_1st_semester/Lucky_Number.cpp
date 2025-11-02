a0c24119a600c507434e221b2db4b4cd348d3b2d
Danail Donkov <danail.donkov5@gmail.com>
2025-11-03 00:24:45 +0200

    Create Lucky_Number.cpp

---
+++ Term_Paper_1st_semester/Lucky_Number.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    int N;
+
+    
+    do {
+        cout << "Enter a number between 1 and 999: ";
+        cin >> N;
+
+        if (N < 1 || N > 999) {
+            cout << "Invalid number! Please enter a number between 1 and 999." << endl;
+        }
+
+    } while (N < 1 || N > 999);
+
+    set<int> seenNumbers;
+    cout << N;
+
+    while (true) {
+        if (seenNumbers.count(N)) {
+            cout << endl << "First repeated number: " << N << endl;
+            break;
+        }
+
+        seenNumbers.insert(N);
+
+        int sum = 0, temp = N;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += digit * digit;
+            temp /= 10;
+        }
+
+        N = sum;
+        cout << " -> " << N;
+    }
+
+    return 0;
+}
