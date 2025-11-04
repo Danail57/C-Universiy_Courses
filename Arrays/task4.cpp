c38955478a07a97daf4d6306027b8a927071a1ca
Danail Donkov <danail.donkov5@gmail.com>
2025-11-04 10:01:11 +0200

    Create task4.cpp

---
+++ Arrays/task4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "Enter number for n: ";
+	cin >> n;
+	double x[100], y[100];
+
+	for (int i = 0; i < n; i++)
+		cin >> x[i] >> y[i];
+
+	double total_length = 0;
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		double dx = x[i + 1] - x[i];
+		double dy = y[i + 1] - y[i];
+		total_length += sqrt(dx * dx + dy * dy);
+	}
+	cout << "Total length; " << total_length << endl;
+	return 0;
+}
