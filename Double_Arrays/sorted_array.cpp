c6052aee47f9784d637e8a2a9d9450c1cc3b5096
Danail Donkov <danail.donkov5@gmail.com>
2025-12-01 09:57:22 +0200

    Create sorted_array.cpp

---
+++ Double_Arrays/sorted_array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sorted_array_A;
+	cout << "Enter how many numbers you want: ";
+	cin >> sorted_array_A;
+
+	int* arr = new int[sorted_array_A];
+	cout << "Enter the elements is ascending order: ";
+	int current_number = 0;
+
+	for (int i = 0; i < sorted_array_A; i++)
+}
