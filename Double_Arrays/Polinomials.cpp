8c0f1303586c34af5408454a3e9b2f1f44d5a765
Danail Donkov <danail.donkov5@gmail.com>
2025-12-01 08:55:51 +0200

    Create Polinomials.cpp

---
+++ Double_Arrays/Polinomials.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int power_n;
+	cout << "Enter the power: ";
+	cin >> power_n;
+
+	double* coeff = new double[power_n + 1];
+
+	cout << "Enter the coefficients a0, a1, ..., an:\n";
+
+	for (int i = 0; i <= power_n; i++)
+	{
+		cout << "a[" << i + 1 << "] = ";
+		cin >> coeff[i];
+	}
+
+	double x;
+	cout << "Enter a number for x: ";
+	cin >> x;
+
+	double y = 0;
+
+	for (int i = 0; i <= power_n; i++)
+	{
+		y = y * x + coeff[i];
+		cout << y;
+	}
+	cout << "\nResullt y = " << y << endl;
+	delete[] coeff;
+	return 0;
+}
