e372afe81ea88875f3051e7e9748cf88b5e5aa62
Danail Donkov <danail.donkov5@gmail.com>
2025-12-13 11:38:37 +0200

    Update file.cpp

--- Functions/file.cpp
+++ Functions/file.cpp
@@ -1,33 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
+// Въвеждане на масив
 void in_arr1(int x[], int& n)
 {
-	if (n <= 0)
-	{
-		printf("Enter how many elements you want to type: ");
-		scanf("%d", &n);
-	}
-	for (int i = 0, i < n, i++);
-	{
-		printf("%d element: ", i + 1);
-		scanf("%d", &x[i]);
-	}
+    if (n <= 0)
+    {
+        cout << "Enter how many elements you want to type: ";
+        cin >> n;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << i + 1 << " element: ";
+        cin >> x[i];
+    }
 }
 
+// Сумиране на елементите
 int sum_arr1(int x[], const int n)
 {
-	for (int i = 0, s = 0, i < n, i++); s += x[i];
-	return s;
+    int s = 0;
+    for (int i = 0; i < n; i++)
+        s += x[i];
+    return s;
 }
 
 int main()
 {
-	int r[100], BrEl = 0;
-	in_arr1(r, BrEl);
-	printf("Enter elements: \n");
-	for (int i = 0, i < BrEl, i++);
-		printf("%d", r[i]);
-	printf("\Sum: %d\n", sum_arr1(r, BrEl));
-	return 0;
+    int r[100], BrEl = 0;
+    in_arr1(r, BrEl);
+
+    cout << "Entered elements: ";
+    for (int i = 0; i < BrEl; i++)
+        cout << r[i] << " ";
+    
+    cout << "\nSum: " << sum_arr1(r, BrEl) << endl;
+
+    return 0;
 }
