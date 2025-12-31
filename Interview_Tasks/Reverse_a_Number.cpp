c48f16872a5ef931219265eb169f3b6645771ccf
Danail Donkov <danail.donkov5@gmail.com>
2025-12-31 11:16:31 +0200

    Update Reverse_a_Number.cpp

--- Interview_Tasks/Reverse_a_Number.cpp
+++ Interview_Tasks/Reverse_a_Number.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	int n, digit;
+	int reversed_number = 0;
+	
+	cout << "Enter a number: ";
+	cin >> n;
+
+	while (n > 0)
+	{
+		digit = n % 10;
+		reversed_number = reversed_number * 10 + digit;
+		n = n / 10;
+	}
+	cout << "Reversed number: " << reversed_number << endl;
+	return 0;
+}
