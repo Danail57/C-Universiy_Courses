cd006f19b1282d6eb613d2de4e90d3861a8013c8
Danail Donkov <danail.donkov5@gmail.com>
2026-01-06 12:07:23 +0200

    Update string.length().cpp

--- Strings/Strings_Functions/string.length().cpp
+++ Strings/Strings_Functions/string.length().cpp
@@ -1,1 +1,23 @@
+// string.length() - returns length of a string
 
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string first_name;
+	cout << "Enter your first name: ";
+	getline(cin, first_name);
+
+	if (first_name.length() >= 12)
+	{
+		cout << "Your name can not be over 12 chars long.";
+	}
+	else
+	{
+		cout << "Welcome " << first_name;
+	}
+	cout << endl;
+	return 0;
+}
