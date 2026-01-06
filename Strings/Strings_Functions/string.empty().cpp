57ee3464cb9fde09bbd616a17756105dd435762f
Danail Donkov <danail.donkov5@gmail.com>
2026-01-06 12:09:46 +0200

    Update string.empty().cpp

--- Strings/Strings_Functions/string.empty().cpp
+++ Strings/Strings_Functions/string.empty().cpp
@@ -1,1 +1,23 @@
+// string.empty() - returns true if empty
 
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string first_name;
+	cout << "Enter your first name: ";
+	getline(cin, first_name);
+
+	if (first_name.empty())
+	{
+		cout << "You did not enter a name!";
+	}
+	else
+	{
+		cout << "Welcome " << first_name;
+	}
+	cout << endl;
+	return 0;
+}
