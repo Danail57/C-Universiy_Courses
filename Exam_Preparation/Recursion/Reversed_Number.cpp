5bc6e9169de4ac9502af453626b9b7c8ca8e8637
Danail Donkov <danail.donkov5@gmail.com>
2026-01-08 22:50:31 +0200

    Update Reversed_Number.cpp

--- Exam_Preparation/Recursion/Reversed_Number.cpp
+++ Exam_Preparation/Recursion/Reversed_Number.cpp
@@ -1,1 +1,23 @@
+// Да се състави програма с рекурсивни функции 
+// за извеждане на цифрите на зададено цяло
+// число в прав и обратен ред. 
 
+
+#include <iostream>
+using namespace std;
+
+void print_reverse(int n)
+{
+	if (n == 0)
+		return;
+	cout << n % 10 << " ";
+	print_reverse(n / 10);
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	print_reverse(n);
+	return 0;
+}
