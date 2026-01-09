8fc1c363d1c345739879d76e440ac72d965cc7bf
Danail Donkov <danail.donkov5@gmail.com>
2026-01-09 16:29:19 +0200

    Create Average_Odd_Sum.cpp

---
+++ Exam_Preparation/Recursion/Average_Odd_Sum.cpp
@@ -0,0 +1,70 @@
+//Зададени са два едномерни целoчислeни масива 
+// А0, А1,...,Аn-1 и B0, B1,...,Bn-1. 
+// Да се състави рекурсивна програма, която намира 
+// срeдноаритметичното r създава нов масив 
+// а0, а1,...,аn-1, като стойността на 
+// аi  е равна на разликата между 
+// средно-аритметичното на нечетните цифри
+// на Аi и средно-аритметичното 
+// на нечетните цифри на Bi. 
+
+#include <iostream>
+using namespace std;
+
+double average_odd_digits(int number, int& sum, int& count)
+{
+	if (number == 0)
+	{
+		if (count == 0)
+			return 0;
+		return (double)sum / count;
+	}
+	int digit = number % 10;
+	if (digit % 2 == 1)
+	{
+		sum += digit;
+		count++;
+	}
+	return average_odd_digits(number / 10, sum, count);
+}
+
+void create_difference_array(int A[], int B[], double C[], int n, int i = 0)
+{
+	if (i == n)
+		return;
+	int sum_a = 0, count_a = 0;
+	int sum_b = 0, count_b = 0;
+
+	double average_a = average_odd_digits(A[i], sum_a, count_a);
+	double average_b = average_odd_digits(B[i], sum_b, count_b);
+
+	C[i] = average_a - average_b;
+	create_difference_array(A, B, C, n, i + 1);
+}
+
+int main()
+{
+	int n;
+	cout << "Enter size of arrays: ";
+	cin >> n;
+
+	int A[1000], B[1000];
+	double C[1000];
+
+	cout << "Enter elements of A: ";
+	for (int i = 0; i < n; i++)
+		cin >> A[i];
+
+	cout << "Enter elements of B: ";
+	for (int i = 0; i < n; i++)
+		cin >> B[i];
+
+	create_difference_array(A, B, C, n);
+
+	cout << "Resulting array C: ";
+	for (int i = 0; i < n; i++)
+		cout << C[i] << " ";
+	cout << endl;
+
+	return 0;
+}
