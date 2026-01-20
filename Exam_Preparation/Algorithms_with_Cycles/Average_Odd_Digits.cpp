63fbd031e54aded979fc14e30dc4abee72c37de0
Danail Donkov <danail.donkov5@gmail.com>
2026-01-20 18:37:01 +0200

    Create Average_Odd_Digits.cpp

---
+++ Exam_Preparation/Algorithms_with_Cycles/Average_Odd_Digits.cpp
@@ -0,0 +1,40 @@
+/*  Да се състави алгоритъм, който намира 
+средно-аритметичнто на вторите степени на
+нечетните цифри на зададено цяло число.  
+ */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int number;
+	int sum = 0;
+	int count = 0;
+	double average;
+
+	cout << "Write an integer: ";
+	cin >> number;
+
+	while (number > 0)
+	{
+		int digit = number % 10;
+		if (digit % 2 != 0)
+		{
+			sum += digit * digit;
+			count++;
+		}
+		number /= 10;
+	}
+	if (count > 0)
+	{
+		average = double(sum) / count;
+		cout << "Average = " << average << endl;
+	}
+	else
+	{
+		cout << "No odd digits" << endl;
+	}
+	return 0;
+}
