33d4285205a03c55efea3600ec019473d9e7f61e
Danail Donkov <danail.donkov5@gmail.com>
2026-01-31 13:03:44 +0200

    Update file_2.cpp

--- Debugging Folder/Strings/file_2.cpp
+++ Debugging Folder/Strings/file_2.cpp
@@ -6,7 +6,7 @@ int main()
 {
     char str1[11];
     char str2[] = "1234567890";
-    strncpy (str1, str2, 5);
+    strncpy (str1, str2, 5); // copying the first 5 symbols
     str1[5] = '\0';
     cout << str1 << endl;;
 }
