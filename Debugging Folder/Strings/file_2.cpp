131ce6453befe828a9fc49af2baa2e8f530af2d5
Danail Donkov <danail.donkov5@gmail.com>
2026-01-31 13:02:35 +0200

    Create file_2.cpp

---
+++ Debugging Folder/Strings/file_2.cpp
@@ -0,0 +1,14 @@
+
+#include <cstring>
+#include <iostream>
+using namespace std;
+int main()
+{
+    char str1[11];
+    char str2[] = "1234567890";
+    strncpy (str1, str2, 5);
+    str1[5] = '\0';
+    cout << str1 << endl;;
+}
+
+/12345
