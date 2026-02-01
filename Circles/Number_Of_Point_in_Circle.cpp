d61ac21a113f229fd7f470c0a406d9ffa4a9f121
Danail Donkov <danail.donkov5@gmail.com>
2026-02-01 15:31:27 +0200

    Create Number_Of_Point_in_Circle.cpp

---
+++ Circles/Number_Of_Point_in_Circle.cpp
@@ -0,0 +1,47 @@
+
+/*20) Зададени са k окръжности
+ *(абсциса и ордината на центъра и радиус)
+ *и m точки (абсциса и ордината).
+ *Да се намерят номерата на окръжностите, в
+ *които лежи всяка точка.
+*/
+
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int k, m;
+    double x[1000], y[1000];
+    double xc[1000], yc[1000], r[1000];
+
+    cout << "Enter the number of circles: ";
+    cin >> k;
+    for (int i = 0; i < k; i++) {
+        cout << "Circle " << i + 1 << " (xc yc r): ";
+        cin >> xc[i] >> yc[i] >> r[i];
+    }
+
+    cout << "Enter the number of points: ";
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        cout << "Point " << i + 1 << " (x y): ";
+        cin >> x[i] >> y[i];
+    }
+
+    for (int i = 0; i < m; i++) {
+        cout << "Point " << i + 1 << " (" << x[i] << ", " << y[i] << ") lies in circles: ";
+        bool found = false;
+        for (int j = 0; j < k; j++) {
+            double dx = x[i] - xc[j];
+            double dy = y[i] - yc[j];
+            if (dx * dx + dy * dy <= r[i] * r[i]) {
+                cout << j + 1 << " ";
+                found = true;
+            }
+        }
+        if (!found) cout << "None";
+        cout << endl;
+    }
+}
