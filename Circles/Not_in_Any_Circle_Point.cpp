3f4ac4139737e8ddf8558db50550f8a6fca88b24
Danail Donkov <danail.donkov5@gmail.com>
2026-02-01 16:27:53 +0200

    Create Not_in_Any_Circle_Point.cpp

---
+++ Circles/Not_in_Any_Circle_Point.cpp
@@ -0,0 +1,53 @@
+
+/*19) Зададени са k окръжности
+ *(абсциса и ордината на центъра и радиус)
+ *и m точки (абсциса и ордината).
+ *Да се определи дали има точка,
+ *която лежи в нито една окръжност
+*/
+
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int k, m;
+    double x[1000], y[1000];
+    double xc[1000], yc[1000], r[1000];
+
+    cout << "Enter the number of circles: ";
+    cin >> k;
+    for (int i = 0; i < k; i++) {
+        cout << "Circle " << i + 1 << " (xc yc r): ";
+        cin >> xc[i] >> yc[i] >> r[i];
+    }
+
+    cout << "Enter the number of points: ";
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        cout << "Point " << i + 1 << " (x y): ";
+        cin >> x[i] >> y[i];
+    }
+    bool found = false;
+    for (int i = 0; i < m; i++) {
+        bool inside_any = false;
+
+        for (int j = 0; j < k; j++) {
+            double dx = x[i] - xc[j];
+            double dy = y[i] - yc[j];
+            double dist = dx * dx + dy * dy;
+            if (dist <= r[j] * r[j]) {
+                inside_any = true;
+                break;
+            }
+        }
+        if (!inside_any) {
+            cout << "Point " << i + 1 << " (" << x[i] << ", " << y[i] << ") lies in no circle.\n";
+            found = true;
+        }
+    }
+    if (!found) {
+        cout << "There is no point that lies in any circle.\n";
+    }
+}
