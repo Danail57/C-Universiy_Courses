e59468fc716de85248023ebaa817ada71f493663
Danail Donkov <danail.donkov5@gmail.com>
2026-02-04 22:33:46 +0200

    Create Decreasing_Numbers_and_their_Sum.cpp

---
+++ Exam_Preparation/Decreasing_Numbers_and_their_Sum.cpp
@@ -0,0 +1,45 @@
+
+
+/*17)Да се състави програма, която от цифрите на числата
+ *образува намаляваща нестрого последователност
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << " n = ";
+    cin >> n;
+
+    int A[100];
+    for (int i = 0; i < n; i++) {
+        cout << "A[" << i + 1 << "] = ";
+        cin >> A[i];
+    }
+
+    int sum = 0;
+    cout << "Decreasing digits: " << endl;
+    for (int i = 0; i < n; i++) {
+
+        int x = abs(A[i]);
+        bool decreasing = true;
+        int last_digit = -1;
+
+        while (x > 0) {
+            int digit = x % 10;
+            if (digit < last_digit) {
+                decreasing = false;
+                break;
+            }
+            last_digit = digit;
+            x /= 10;
+        }
+        if (decreasing) {
+            cout << A[i] << endl;
+            sum += A[i];
+        }
+    }
+    cout << "Sum = " << sum << endl;
+}
