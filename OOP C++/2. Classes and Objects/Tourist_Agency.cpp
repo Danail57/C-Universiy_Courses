8d9a03dc0c00c8ea8ec2b2a286bf2cf4136d8407
Danail Donkov <danail.donkov5@gmail.com>
2026-03-12 22:57:58 +0200

    Update Tourist_Agency.cpp

--- OOP C++/2. Classes and Objects/Tourist_Agency.cpp
+++ OOP C++/2. Classes and Objects/Tourist_Agency.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "tourist_agency.h"
 using namespace std;
-
 
 class TouristAgency {
 private:
@@ -12,90 +10,54 @@ private:
 
 public:
 	string name;
-	int phone_number;
+	string phone_number;
 	string destination;
 
 	void get_data();
 	void display();
-	string getID();
+	string getID() const;
+	int get_salary() const;
 };
 
-string TouristAgency::getID()
+void display_id_of_women(const vector<TouristAgency>& people);
+long month_income_above_1000(const vector<TouristAgency>& people);
+
+string TouristAgency::getID() const
 {
 	return ID;
 }
 
+int TouristAgency::get_salary() const
+{
+	return month_salary;
+}
+
 void TouristAgency::get_data()
 {
+	cin.ignore(1000, '\n');
 	cout << "What destination you want to visit?"; cin >> destination;
 	cout << "Phone number: "; cin >> phone_number;
-	cout << "Name: "; cin >> name;
+	cout << "Name: "; cin.ignore(); getline(cin, name);
 	cout << "ID: "; cin >> ID;
 	cout << "Month salary: "; cin >> month_salary;
 }
 
-
-
 void TouristAgency::display()
 {
-	cin.ignore();
 	cout << "Chosen destination: " << destination << endl;
 	cout << "Guest name: " << name << endl;
 	cout << "Phone number: " << phone_number << endl;
 }
 
-
-// vector <TouristAgency> information
-void record_all_guest_data(TouristAgency information[], int size)
+void display_id_of_women(const vector<TouristAgency>& people)
 {
-	vector <TouristAgency> people;
-	for (int i = 0; i < size; i++)
-	{
-		people.push_back(information[i]);
-	}
-
-	display_id_of_women(people);
-	long income = month_income_above_1000(information, size);
-	cout << "Total income above 1000: " << income << endl;
-}
-
-
-int get_birthday_dates_of_women(vector<TouristAgency> people)
-{
-	display_id_of_women(people); 
-}
-
-void display_id_of_women(vector<TouristAgency> people)
-{
-	/*vector <char> women_ids;
-
-	int day, month, year;
-	cout << day << "." << month << "." << year << "." << endl;
-	cin >> day >> month >> year;
-
-	char ld = '8';
-	char id;
-	int women_count = 0;
-	for (auto w : women_ids)
-	{
-		cout << "Enter ID: " << w << endl;
-		cin >> id;
-
-		char last_digit = ld;
-		if ((last_digit - '0') % 2 == 0)
-		{
-			women_count++;
-		}
-	}
-	return */
-
 	int size = people.size();
 	for (int i = 0; i < size; i++)
 	{
-		TouristAgency currPerson = people[i];
+		const TouristAgency& currPerson = people[i];
 		string currPerson_ID = currPerson.getID();
 		bool isWoman = false;
-		if (currPerson_ID[7] % 2 == 1) { isWoman = true; }
+		if ((currPerson_ID[7] - '0') % 2 == 1) { isWoman = true; }
 		if (isWoman)
 		{
 			cout << "ID of woman #" << i + 1 << ": " << currPerson_ID << '\n';
@@ -104,36 +66,60 @@ void display_id_of_women(vector<TouristAgency> people)
 }
 
 
-long month_income_above_1000(TouristAgency salaries[], int size)
+long month_income_above_1000(const vector<TouristAgency>& people)
 {
 	long salary = 0;
-
-	vector <int> salaries_above_1000;
-	for (auto i : salaries_above_1000)
+	for (const auto& person : people)
 	{
-		if (salaries_above_1000[i] > 1000)
+		if (person.get_salary() > 1000)
 		{
-			salary += salaries_above_1000[i];
+			salary += person.get_salary();
 		}
 	}
 	return salary;
 }
 
 
+void get_birthday_dates_of_women(vector<TouristAgency> people)
+{
+	display_id_of_women(people);
+}
+
 int main()
 {
-	// create vector
 	vector<TouristAgency> people{};
+	int choice;
 
-	// menu
-	
-	// 1. add
-	for (int i = 0; i < 5; i++)
+	do 
 	{
-		TouristAgency toBeAdded{};
-		toBeAdded.get_data();
-		people.push_back(toBeAdded);
-	}
+		cout << "\nMENU\n";
+		cout << "1. Add tourist\n";
+		cout << "2. Show women IDs\n";
+		cout << "3. Total salary above 1000\n";
+		cout << "4. Exit\n";
+		cout << "Choice: ";
 
-	display_id_of_women(people);
+		if (!(cin >> choice))
+		{
+			cout << "Invalid input! Use numbers only.\n";
+			break;
+		}
+
+		if (choice == 1)
+		{
+			TouristAgency t;
+			t.get_data();
+			people.push_back(t);
+		}
+		else if (choice == 2)
+		{
+			display_id_of_women(people);
+		}
+		else if (choice == 3)
+		{
+			cout << "Total salary above 1000: " << month_income_above_1000(people) << endl;
+		}
+
+	} while (choice != 4);
+	return 0;
 }
